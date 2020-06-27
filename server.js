9ab118f8f5a1387ca841a5ba1d6cfb8f8a4734ae
fanatic75 <prateekbanga12@gmail.com>
2020-06-27 16:35:16 +0530

    added compression for heroku server

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 exports.__esModule = true;
 var express = require("express");
 var path = require("path");
+var compression = require("compression");
 var app = express();
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(compression());
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
