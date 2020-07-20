100a6cdb9fe8113560c9bdc2604cbd95ba73c34f
fanatic75 <prateekbanga12@gmail.com>
2020-07-20 14:00:00 +0530

    bug fix, added analytics in useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./Routes";
 import "./styles/App.css";
@@ -9,17 +9,19 @@ export const  ThemeContext = React.createContext("dark-mode");
 
 const App: React.FC = () => {
  
-  firebase.initializeApp({
-    apiKey: "AIzaSyBi36Ln28QqyzmU-o4EHXspMAdHWi0bfow",
-    authDomain: "portfolio-29a59.firebaseapp.com",
-    databaseURL: "https://portfolio-29a59.firebaseio.com",
-    projectId: "portfolio-29a59",
-    storageBucket: "portfolio-29a59.appspot.com",
-    messagingSenderId: "409595576323",
-    appId: "1:409595576323:web:3d26cab2e6c20a854ace36",
-    measurementId: "G-39PW8DPL6G"
+  useState(()=>{
+    firebase.initializeApp({
+      apiKey: "AIzaSyBi36Ln28QqyzmU-o4EHXspMAdHWi0bfow",
+      authDomain: "portfolio-29a59.firebaseapp.com",
+      databaseURL: "https://portfolio-29a59.firebaseio.com",
+      projectId: "portfolio-29a59",
+      storageBucket: "portfolio-29a59.appspot.com",
+      messagingSenderId: "409595576323",
+      appId: "1:409595576323:web:3d26cab2e6c20a854ace36",
+      measurementId: "G-39PW8DPL6G"
+    });
+    firebase.analytics(); 
   });
-  firebase.analytics();
   
   const darkMode = useDarkMode();
   return (
