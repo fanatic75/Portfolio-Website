3a34fa4d5de78d1e9600516e6a1f6aa541b5a4f1
Prateek Banga <30731059+fanatic75@users.noreply.github.com>
2022-05-23 11:08:42 +0530

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types";
 import { useMedia } from "use-media";
 import Header from "./header"
