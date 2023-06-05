ca926587beea540110dfded95f5a691019b014d4
Prateek Banga <30731059+fanatic75@users.noreply.github.com>
2023-06-05 23:21:01 +0530

    Update Routes.tsx

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
+
 import Contact from "./pages/Contact";
 import Portfolio from "./pages/Portfolio";
 
